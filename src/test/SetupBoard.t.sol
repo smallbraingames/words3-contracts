68011bcd5b97f76c606d46bcedff6095307511f6
smallbraingames <smallbraindev@gmail.com>
2023-04-24 03:28:57 -0700

    feat: simple setup board test

--- src/test/SetupBoard.t.sol
+++ src/test/SetupBoard.t.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import {IWorld} from "codegen/world/IWorld.sol";
+import {TileTable, TileTableData} from "codegen/Tables.sol";
+import {Letter} from "codegen/Types.sol";
+
+import {Coord} from "common/Coord.sol";
+
 import "forge-std/Test.sol";
 import {MudV2Test} from "@latticexyz/std-contracts/src/test/MudV2Test.t.sol";
 import {getKeysWithValue} from "@latticexyz/world/src/modules/keyswithvalue/getKeysWithValue.sol";
 
-import {IWorld} from "codegen/world/IWorld.sol";
-import {TileTable} from "codegen/Tables.sol";
-
-contract CounterTest is MudV2Test {
+contract SetupBoardTest is MudV2Test {
     IWorld world;
 
     function setUp() public override {
@@ -25,26 +28,11 @@ contract CounterTest is MudV2Test {
         assertTrue(codeSize > 0);
     }
 
-    // function testCounter() public {
-    //     // Expect the counter to be 1 because it was incremented in the PostDeploy script.
-    //     bytes32 key = SingletonKey;
-    //     uint32 counter = CounterTable.get(world, key);
-    //     assertEq(counter, 1);
-
-    //     // Expect the counter to be 2 after calling increment.
-    //     world.increment();
-    //     counter = CounterTable.get(world, key);
-    //     assertEq(counter, 2);
-    // }
-
-    // function testKeysWithValue() public {
-    //     bytes32 key = SingletonKey;
-    //     uint32 counter = CounterTable.get(world, key);
-    //     bytes32[] memory keysWithValue = getKeysWithValue(
-    //         world,
-    //         CounterTableTableId,
-    //         CounterTable.encode(counter)
-    //     );
-    //     assertEq(keysWithValue.length, 1);
-    // }
+    function testFirstWord() public {
+        world.playFirstWord();
+        TileTableData memory firstTile = TileTable.get(world, 0, 0);
+        assertEq(uint8(firstTile.letter), uint8(Letter.I));
+        TileTableData memory thirdTile = TileTable.get(world, 2, 0);
+        assertEq(uint8(thirdTile.letter), uint8(Letter.F));
+    }
 }
