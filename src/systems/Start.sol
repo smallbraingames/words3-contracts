43db462aee0c5a56e250a4f84b380b7f100630f8
smallbraingames <smallbraindev@gmail.com>
2023-06-04 19:54:35 -0700

    feat: start game system

---
+++ src/systems/Start.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import {Letter, Status} from "codegen/Types.sol";
+
+import {Coord} from "common/Coord.sol";
+import {GameStartedOrOver} from "common/Errors.sol";
+import {LibTile} from "libraries/LibTile.sol";
+import {LibGame} from "libraries/LibGame.sol";
+
+import {System} from "@latticexyz/world/src/System.sol";
+
+contract Start is System {
+    function start(Letter[] memory initialWord, uint16 maxWords, bytes32 merkleRoot) public {
+        if (LibGame.getGameStatus() != Status.NOT_STARTED) {
+            revert GameStartedOrOver();
+        }
+        for (uint256 i = 0; i < initialWord.length; i++) {
+            Coord memory coord = Coord({x: int32(uint32(i)), y: 0});
+            LibTile.setTile(coord, initialWord[i], address(0));
+        }
+        LibGame.startGame(maxWords, merkleRoot);
+    }
+}
