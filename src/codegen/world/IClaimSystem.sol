f3b6b00d14a00cfdc07f2b813b627dad477a4437
smallbraingames <smallbraindev@gmail.com>
2024-04-07 22:40:19 +0900

    feat: claim whenever & deduct points

--- src/codegen/world/IClaimSystem.sol
+++ src/codegen/world/IClaimSystem.sol
@@ -9,8 +9,9 @@ pragma solidity >=0.8.24;
  * @dev This interface is automatically generated from the corresponding system contract. Do not edit manually.
  */
 interface IClaimSystem {
-    error AlreadyClaimed();
-    error InvalidClaimAddress();
+  error AlreadyClaimed();
+  error InvalidClaimAddress();
+  error NotEnoughPoints();
 
-    function claim(address player) external;
+  function claim(uint32 points) external;
 }
