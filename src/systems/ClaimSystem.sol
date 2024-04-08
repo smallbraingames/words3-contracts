0791ef9f36538fda20d1881c0987b538f431b950
smallbraingames <smallbraindev@gmail.com>
2024-04-08 23:19:11 +0900

    fix: decrement after claim in claimsystem

--- src/systems/ClaimSystem.sol
+++ src/systems/ClaimSystem.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.0;
 
 import { System } from "@latticexyz/world/src/System.sol";
 import { Points } from "codegen/index.sol";
-import { SINGLETON_ADDRESS } from "common/Constants.sol";
 
 import { LibPlayer } from "libraries/LibPlayer.sol";
 import { LibTreasury } from "libraries/LibTreasury.sol";
@@ -16,17 +15,13 @@ contract ClaimSystem is System {
     function claim(uint32 points) public {
         address player = _msgSender();
 
-        if (player == address(0) || player == SINGLETON_ADDRESS) {
-            revert InvalidClaimAddress();
-        }
-
         uint32 playerPoints = Points.get(player);
         if (points > playerPoints) {
             revert NotEnoughPoints();
         }
 
-        LibPlayer.decrementPoints(player, points);
         uint256 claimAmount = LibTreasury.getClaimAmount(points);
+        LibPlayer.decrementPoints(player, points);
 
         payable(player).transfer(claimAmount);
     }
