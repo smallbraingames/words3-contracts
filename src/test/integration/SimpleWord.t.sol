aafffe74b5f163f1af126e5a805452730a4bff15
smallbraingames <smallbraindev@gmail.com>
2024-04-24 23:28:13 -0500

    feat: transfer test

--- src/test/integration/SimpleWord.t.sol
+++ src/test/integration/SimpleWord.t.sol
@@ -1,115 +1,125 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity >=0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+/* solhint-disable no-global-import */
+/* solhint-disable func-name-mixedcase */
 
-// import { Direction, Letter } from "codegen/common.sol";
-// import { MerkleRootConfig, TileLetter, TilePlayer } from "codegen/index.sol";
-// import { IWorld } from "codegen/world/IWorld.sol";
+import { Words3Test } from "../Words3Test.t.sol";
+import { Merkle } from "../murky/src/Merkle.sol";
+import { Direction, Letter } from "codegen/common.sol";
+import { TileLetter } from "codegen/index.sol";
+import { IWorld } from "codegen/world/IWorld.sol";
+import { Bound } from "common/Bound.sol";
+import { Coord } from "common/Coord.sol";
+import "forge-std/Test.sol";
 
-// import { Bound } from "common/Bound.sol";
-// import { Coord } from "common/Coord.sol";
-// import { GameStartedOrOver } from "common/Errors.sol";
+contract SimpleWord is Words3Test {
+    bytes32[] private words;
+    Merkle private m;
+    Letter[] private initialWord;
 
-// import { Words3Test } from "../Words3Test.t.sol";
-// import { Merkle } from "../murky/src/Merkle.sol";
-// import "forge-std/Test.sol";
+    function setUp() public override {
+        super.setUp();
+        world = IWorld(worldAddress);
 
-// contract SimpleWord is Words3Test {
-//     IWorld world;
-//     bytes32[] public words;
-//     Merkle private m;
-//     Letter[] initialWord;
+        m = new Merkle();
+        Letter[] memory hi = new Letter[](2);
+        hi[0] = Letter.H;
+        hi[1] = Letter.I;
+        Letter[] memory go = new Letter[](2);
+        go[0] = Letter.G;
+        go[1] = Letter.O;
+        words.push(keccak256(bytes.concat(keccak256(abi.encode(hi))))); // hi
+        words.push(keccak256(bytes.concat(keccak256(abi.encode(go))))); // go
 
-//     function setUp() public override {
-//         super.setUp();
-//         world = IWorld(worldAddress);
+        setDefaultLetterOdds();
+    }
 
-//         m = new Merkle();
-//         Letter[] memory hi = new Letter[](2);
-//         hi[0] = Letter.H;
-//         hi[1] = Letter.I;
-//         Letter[] memory go = new Letter[](2);
-//         go[0] = Letter.G;
-//         go[1] = Letter.O;
-//         words.push(keccak256(bytes.concat(keccak256(abi.encode(hi))))); // hi
-//         words.push(keccak256(bytes.concat(keccak256(abi.encode(go))))); // go
+    function test_Setup() public {
+        initialWord = new Letter[](2);
+        initialWord[0] = Letter.H;
+        initialWord[1] = Letter.I;
+        world.start({
+            initialWord: initialWord,
+            merkleRoot: m.getRoot(words),
+            vrgdaTargetPrice: 1e14,
+            vrgdaPriceDecay: 2e17,
+            vrgdaPerDayInitial: 100e18,
+            vrgdaPower: 2e18,
+            crossWordRewardFraction: 3,
+            bonusDistance: 10,
+            numDrawLetters: 7
+        });
+        assertEq(uint8(TileLetter.get(0, 0)), uint8(Letter.H));
+        assertEq(uint8(TileLetter.get(1, 0)), uint8(Letter.I));
+    }
 
-//         setDefaultLetterOdds();
-//     }
+    function test_PlayHi() public {
+        initialWord = new Letter[](2);
+        initialWord[0] = Letter.H;
+        initialWord[1] = Letter.I;
+        world.start({
+            initialWord: initialWord,
+            merkleRoot: m.getRoot(words),
+            vrgdaTargetPrice: 12e14,
+            vrgdaPriceDecay: 2e17,
+            vrgdaPerDayInitial: 10e18,
+            vrgdaPower: 2e18,
+            crossWordRewardFraction: 3,
+            bonusDistance: 3,
+            numDrawLetters: 8
+        });
 
-//     function test_Setup() public {
-//         initialWord = new Letter[](2);
-//         initialWord[0] = Letter.H;
-//         initialWord[1] = Letter.I;
-//         world.start(initialWord, m.getRoot(words), 0, 0, 0, 1e16, 3, 5);
-//         assertEq(uint8(TileLetter.get(0, 0)), uint8(Letter.H));
-//         assertEq(uint8(TileLetter.get(1, 0)), uint8(Letter.I));
-//     }
+        Letter[] memory word = new Letter[](2);
+        word[0] = Letter.EMPTY;
+        word[1] = Letter.I;
+        Bound[] memory bounds = new Bound[](2);
+        bytes32[] memory proof = m.getProof(words, 0);
 
-//     function test_PlayHi() public {
-//         initialWord = new Letter[](2);
-//         initialWord[0] = Letter.H;
-//         initialWord[1] = Letter.I;
-//         world.start({
-//             initialWord: initialWord,
-//             merkleRoot: m.getRoot(words),
-//             vrgdaTargetPrice: 1,
-//             vrgdaPriceDecay: 1e17,
-//             vrgdaPerDayInitial: 100e18,
-//             vrgdaPower: 1e16,
-//             crossWordRewardFraction: 3,
-//             bonusDistance: 5
-//         });
+        address player = address(0x123);
+        vm.deal(player, 50 ether);
+        vm.startPrank(player);
+        for (uint256 i = 0; i < 50; i++) {
+            uint256 price = world.getDrawPrice();
+            vm.warp(block.timestamp + 1 days);
+            world.draw{ value: price }(player);
+        }
+        world.play(word, proof, Coord({ x: 0, y: 0 }), Direction.TOP_TO_BOTTOM, bounds);
+        vm.stopPrank();
+    }
 
-//         Letter[] memory word = new Letter[](2);
-//         word[0] = Letter.EMPTY;
-//         word[1] = Letter.I;
-//         Bound[] memory bounds = new Bound[](2);
-//         bytes32[] memory proof = m.getProof(words, 0);
+    function test_MultipleDraws() public {
+        world.start({
+            initialWord: initialWord,
+            merkleRoot: m.getRoot(words),
+            vrgdaTargetPrice: 1e14,
+            vrgdaPriceDecay: 2e17,
+            vrgdaPerDayInitial: 100e18,
+            vrgdaPower: 2e18,
+            crossWordRewardFraction: 3,
+            bonusDistance: 10,
+            numDrawLetters: 7
+        });
 
-//         address player = address(0x123);
-//         vm.deal(player, 50 ether);
-//         vm.startPrank(player);
-//         for (uint256 i = 0; i < 50; i++) {
-//             uint256 price = world.getDrawPrice();
-//             vm.warp(block.timestamp + 1 days);
-//             world.draw{ value: price }(player);
-//         }
-//         world.play(word, proof, Coord({ x: 0, y: 0 }), Direction.TOP_TO_BOTTOM, bounds);
-//         vm.stopPrank();
-//     }
+        address player = address(0x123);
+        vm.deal(player, 50 ether);
+        vm.startPrank(player);
 
-//     function test_MultipleDraws() public {
-//         world.start({
-//             initialWord: initialWord,
-//             merkleRoot: 0xacd24e8edae5cf4cdbc3ce0c196a670cbea1dbf37576112b0a3defac3318b432,
-//             vrgdaTargetPrice: 40e13,
-//             vrgdaPriceDecay: 99_999e13,
-//             vrgdaPerDayInitial: 700e18,
-//             vrgdaPower: 1e18,
-//             crossWordRewardFraction: 3,
-//             bonusDistance: 10
-//         });
-
-//         address player = address(0x123);
-//         vm.deal(player, 50 ether);
-//         vm.startPrank(player);
-
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         vm.warp(block.timestamp + 5);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         vm.warp(block.timestamp + 10);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         vm.warp(block.timestamp + 10);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         vm.warp(block.timestamp + 3);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//         world.draw{ value: world.getDrawPrice() }(player);
-//     }
-// }
+        world.draw{ value: world.getDrawPrice() }(player);
+        world.draw{ value: world.getDrawPrice() }(player);
+        vm.warp(block.timestamp + 5);
+        world.draw{ value: world.getDrawPrice() }(player);
+        vm.warp(block.timestamp + 10);
+        world.draw{ value: world.getDrawPrice() }(player);
+        world.draw{ value: world.getDrawPrice() }(player);
+        world.draw{ value: world.getDrawPrice() }(player);
+        vm.warp(block.timestamp + 10);
+        world.draw{ value: world.getDrawPrice() }(player);
+        world.draw{ value: world.getDrawPrice() }(player);
+        world.draw{ value: world.getDrawPrice() }(player);
+        world.draw{ value: world.getDrawPrice() }(player);
+        world.draw{ value: world.getDrawPrice() }(player);
+        vm.warp(block.timestamp + 3);
+        world.draw{ value: world.getDrawPrice() }(player);
+        world.draw{ value: world.getDrawPrice() }(player);
+    }
+}
