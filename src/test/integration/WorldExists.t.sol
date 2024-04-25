56aaf57c650557a5d90f1e63db3c54f8a1d3b1e9
smallbraingames <smallbraindev@gmail.com>
2024-04-24 23:12:46 -0500

    feat: cross word point counting test

--- src/test/integration/WorldExists.t.sol
+++ src/test/integration/WorldExists.t.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
-
-import { IWorld } from "codegen/world/IWorld.sol";
+pragma solidity >=0.8.24;
+/* solhint-disable no-global-import */
+/* solhint-disable func-name-mixedcase */
 
 import { Words3Test } from "../Words3Test.t.sol";
 import "forge-std/Test.sol";
 
 contract WorldExists is Words3Test {
-    function testWorldExists() public {
+    function test_WorldExists() public {
         uint256 codeSize;
         address addr = worldAddress;
         assembly {
