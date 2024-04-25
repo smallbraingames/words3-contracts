a2952c705042e20c2f69423d5e8dfc243a47f06f
smallbraingames <smallbraindev@gmail.com>
2024-04-24 23:19:00 -0500

    feat: point counting tests

--- src/test/integration/Points.t.sol
+++ src/test/integration/Points.t.sol
@@ -1,138 +1,174 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity >=0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+/* solhint-disable no-global-import */
+/* solhint-disable func-name-mixedcase */
 
-// import { BonusType, Direction, Letter } from "codegen/common.sol";
-// import { MerkleRootConfig, Points, TileLetter, TilePlayer } from "codegen/index.sol";
-// import { IWorld } from "codegen/world/IWorld.sol";
+import { Words3Test } from "../Words3Test.t.sol";
+import { Merkle } from "../murky/src/Merkle.sol";
+import { BonusType, Direction, Letter } from "codegen/common.sol";
+import { Points } from "codegen/index.sol";
+import { IWorld } from "codegen/world/IWorld.sol";
+import { Bonus } from "common/Bonus.sol";
+import { Bound } from "common/Bound.sol";
+import { Coord } from "common/Coord.sol";
+import "forge-std/Test.sol";
+import { LibBonus } from "libraries/LibBonus.sol";
 
-// import { Bonus } from "common/Bonus.sol";
-// import { Bound } from "common/Bound.sol";
-// import { Coord } from "common/Coord.sol";
+contract PointsTest is Words3Test {
+    bytes32[] public words;
+    Merkle private m;
 
-// import { GameStartedOrOver } from "common/Errors.sol";
-// import { LibBonus } from "libraries/LibBonus.sol";
-// import { LibPoints } from "libraries/LibPoints.sol";
+    function setUp() public override {
+        super.setUp();
+        world = IWorld(worldAddress);
+        m = new Merkle();
+        Letter[] memory hi = new Letter[](2);
+        hi[0] = Letter.H;
+        hi[1] = Letter.I;
+        Letter[] memory hello = new Letter[](5);
+        hello[0] = Letter.H;
+        hello[1] = Letter.E;
+        hello[2] = Letter.L;
+        hello[3] = Letter.L;
+        hello[4] = Letter.O;
+        Letter[] memory zone = new Letter[](4);
+        zone[0] = Letter.Z;
+        zone[1] = Letter.O;
+        zone[2] = Letter.N;
+        zone[3] = Letter.E;
+        Letter[] memory zones = new Letter[](5);
+        zones[0] = Letter.Z;
+        zones[1] = Letter.O;
+        zones[2] = Letter.N;
+        zones[3] = Letter.E;
+        zones[4] = Letter.S;
+        Letter[] memory ollie = new Letter[](5);
+        ollie[0] = Letter.O;
+        ollie[1] = Letter.L;
+        ollie[2] = Letter.L;
+        ollie[3] = Letter.I;
+        ollie[4] = Letter.E;
+        words.push(keccak256(bytes.concat(keccak256(abi.encode(hi))))); // hi
+        words.push(keccak256(bytes.concat(keccak256(abi.encode(hello))))); // hello
+        words.push(keccak256(bytes.concat(keccak256(abi.encode(zone))))); // zone
+        words.push(keccak256(bytes.concat(keccak256(abi.encode(zones))))); // zones
+        words.push(keccak256(bytes.concat(keccak256(abi.encode(ollie))))); // ollie
 
-// import { Words3Test } from "../Words3Test.t.sol";
-// import { Merkle } from "../murky/src/Merkle.sol";
-// import "forge-std/Test.sol";
+        setDefaultLetterOdds();
+    }
 
-// contract PointsTest is Words3Test {
-//     IWorld world;
-//     bytes32[] public words;
-//     Merkle private m;
+    function test_CountPoints() public {
+        // Test works as long as points are not on bonus tiles
+        address player1 = address(0x12345);
+        address player2 = address(0x22345);
 
-//     function setUp() public override {
-//         super.setUp();
-//         world = IWorld(worldAddress);
-//         m = new Merkle();
-//         Letter[] memory hi = new Letter[](2);
-//         hi[0] = Letter.H;
-//         hi[1] = Letter.I;
-//         Letter[] memory hello = new Letter[](5);
-//         hello[0] = Letter.H;
-//         hello[1] = Letter.E;
-//         hello[2] = Letter.L;
-//         hello[3] = Letter.L;
-//         hello[4] = Letter.O;
-//         Letter[] memory zone = new Letter[](4);
-//         zone[0] = Letter.Z;
-//         zone[1] = Letter.O;
-//         zone[2] = Letter.N;
-//         zone[3] = Letter.E;
-//         Letter[] memory zones = new Letter[](5);
-//         zones[0] = Letter.Z;
-//         zones[1] = Letter.O;
-//         zones[2] = Letter.N;
-//         zones[3] = Letter.E;
-//         zones[4] = Letter.S;
-//         Letter[] memory ollie = new Letter[](5);
-//         ollie[0] = Letter.O;
-//         ollie[1] = Letter.L;
-//         ollie[2] = Letter.L;
-//         ollie[3] = Letter.I;
-//         ollie[4] = Letter.E;
-//         words.push(keccak256(bytes.concat(keccak256(abi.encode(hi))))); // hi
-//         words.push(keccak256(bytes.concat(keccak256(abi.encode(hello))))); // hello
-//         words.push(keccak256(bytes.concat(keccak256(abi.encode(zone))))); // zone
-//         words.push(keccak256(bytes.concat(keccak256(abi.encode(zones))))); // zones
-//         words.push(keccak256(bytes.concat(keccak256(abi.encode(ollie))))); // ollie
-//     }
+        Letter[] memory initialWord = new Letter[](5);
+        initialWord[0] = Letter.H;
+        initialWord[1] = Letter.E;
+        initialWord[2] = Letter.L;
+        initialWord[3] = Letter.L;
+        initialWord[4] = Letter.O;
 
-//     function testCountPoints() public {
-//         // Test works as long as points are not on bonus tiles
-//         address player1 = address(0x12345);
-//         address player2 = address(0x22345);
+        world.start({
+            initialWord: initialWord,
+            merkleRoot: m.getRoot(words),
+            vrgdaTargetPrice: 1e14,
+            vrgdaPriceDecay: 2e17,
+            vrgdaPerDayInitial: 100e18,
+            vrgdaPower: 2e18,
+            crossWordRewardFraction: 3,
+            bonusDistance: 10,
+            numDrawLetters: 7
+        });
 
-//         Letter[] memory initialWord = new Letter[](5);
-//         initialWord[0] = Letter.H;
-//         initialWord[1] = Letter.E;
-//         initialWord[2] = Letter.L;
-//         initialWord[3] = Letter.L;
-//         initialWord[4] = Letter.O;
+        Letter[] memory word = new Letter[](4);
+        word[0] = Letter.Z;
+        word[1] = Letter.EMPTY;
+        word[2] = Letter.N;
+        word[3] = Letter.E;
 
-//         world.start(initialWord, block.timestamp + 1e6, m.getRoot(words), 0, 1e17, 3e18, 1e16, 3, 5);
+        Bound[] memory bounds = new Bound[](4);
+        bytes32[] memory proof = m.getProof(words, 2);
 
-//         Letter[] memory word = new Letter[](4);
-//         word[0] = Letter.Z;
-//         word[1] = Letter.EMPTY;
-//         word[2] = Letter.N;
-//         word[3] = Letter.E;
+        // Play zone
+        vm.deal(player1, 50 ether);
+        vm.startPrank(player1);
+        for (uint256 i = 0; i < 50; i++) {
+            vm.warp(block.timestamp + 1 days);
+            world.draw{ value: 1 ether }(player1);
+        }
+        world.play(word, proof, Coord({ x: 4, y: -1 }), Direction.TOP_TO_BOTTOM, bounds);
+        vm.stopPrank();
+        assertEq(Points.get(player1), 13);
 
-//         Bound[] memory bounds = new Bound[](4);
-//         bytes32[] memory proof = m.getProof(words, 2);
+        // Play zones
+        Letter[] memory ext = new Letter[](5);
+        ext[0] = Letter.EMPTY;
+        ext[1] = Letter.EMPTY;
+        ext[2] = Letter.EMPTY;
+        ext[3] = Letter.EMPTY;
+        ext[4] = Letter.S;
+        Bound[] memory extBounds = new Bound[](5);
+        bytes32[] memory extProof = m.getProof(words, 3);
 
-//         // Play zone
-//         vm.prank(player1);
-//         world.play(word, proof, Coord({ x: 4, y: -1 }), Direction.TOP_TO_BOTTOM, bounds);
-//         assertEq(Points.get(player1), 13);
+        vm.deal(player2, 50 ether);
+        vm.startPrank(player2);
+        for (uint256 i = 0; i < 50; i++) {
+            vm.warp(block.timestamp + 1 days);
+            world.draw{ value: 1 ether }(player2);
+        }
+        world.play(ext, extProof, Coord({ x: 4, y: -1 }), Direction.TOP_TO_BOTTOM, extBounds);
+        vm.stopPrank();
+        assertEq(Points.get(player2), 14);
 
-//         // Play zones
-//         Letter[] memory ext = new Letter[](5);
-//         ext[0] = Letter.EMPTY;
-//         ext[1] = Letter.EMPTY;
-//         ext[2] = Letter.EMPTY;
-//         ext[3] = Letter.EMPTY;
-//         ext[4] = Letter.S;
-//         Bound[] memory extBounds = new Bound[](5);
-//         bytes32[] memory extProof = m.getProof(words, 3);
-//         vm.prank(player2);
-//         world.play(ext, extProof, Coord({ x: 4, y: -1 }), Direction.TOP_TO_BOTTOM, extBounds);
-//         assertEq(Points.get(player2), 14);
+        // We lose 1 because of rounding
+        assertEq(Points.get(player1), 13 + 4);
+    }
 
-//         // We lose 1 because of rounding
-//         assertEq(Points.get(player1), 13 + 4);
-//     }
+    function test_Bonus() public {
+        Letter[] memory initialWord = new Letter[](5);
+        initialWord[0] = Letter.H;
+        initialWord[1] = Letter.E;
+        initialWord[2] = Letter.L;
+        initialWord[3] = Letter.L;
+        initialWord[4] = Letter.O;
 
-//     function testBonus() public {
-//         Letter[] memory initialWord = new Letter[](5);
-//         initialWord[0] = Letter.H;
-//         initialWord[1] = Letter.E;
-//         initialWord[2] = Letter.L;
-//         initialWord[3] = Letter.L;
-//         initialWord[4] = Letter.O;
+        world.start({
+            initialWord: initialWord,
+            merkleRoot: m.getRoot(words),
+            vrgdaTargetPrice: 1e14,
+            vrgdaPriceDecay: 2e17,
+            vrgdaPerDayInitial: 100e18,
+            vrgdaPower: 2e18,
+            crossWordRewardFraction: 3,
+            bonusDistance: 5,
+            numDrawLetters: 7
+        });
 
-//         world.start(initialWord, block.timestamp + 1e6, m.getRoot(words), 0, 1e17, 3e18, 1e16, 3, 5);
+        Letter[] memory word = new Letter[](5);
+        word[0] = Letter.EMPTY;
+        word[1] = Letter.L;
+        word[2] = Letter.L;
+        word[3] = Letter.I;
+        word[4] = Letter.E;
 
-//         Letter[] memory word = new Letter[](5);
-//         word[0] = Letter.EMPTY;
-//         word[1] = Letter.L;
-//         word[2] = Letter.L;
-//         word[3] = Letter.I;
-//         word[4] = Letter.E;
+        Bound[] memory bounds = new Bound[](5);
+        bytes32[] memory proof = m.getProof(words, 4);
 
-//         Bound[] memory bounds = new Bound[](5);
-//         bytes32[] memory proof = m.getProof(words, 4);
+        vm.deal(address(this), 50 ether);
+        for (uint256 i = 0; i < 50; i++) {
+            vm.warp(block.timestamp + 1 days);
+            world.draw{ value: 1 ether }(address(this));
+        }
+        world.play(word, proof, Coord({ x: 4, y: 0 }), Direction.TOP_TO_BOTTOM, bounds);
 
-//         world.play(word, proof, Coord({ x: 4, y: 0 }), Direction.TOP_TO_BOTTOM, bounds);
-
-//         uint32 truePoints = 5;
-//         Bonus memory bonus = LibBonus.getTileBonus(Coord({ x: 4, y: 4 }));
-//         if (bonus.bonusType == BonusType.MULTIPLY_WORD) {
-//             truePoints *= bonus.bonusValue;
-//         } else {
-//             truePoints += bonus.bonusValue - 1;
-//         }
-//         assertEq(Points.get(address(this)), truePoints);
-//     }
-// }
+        uint32 truePoints = 5;
+        Bonus memory bonus = LibBonus.getTileBonus(Coord({ x: 4, y: 4 }));
+        if (bonus.bonusType == BonusType.MULTIPLY_WORD) {
+            truePoints *= bonus.bonusValue;
+        } else {
+            truePoints += bonus.bonusValue - 1;
+        }
+        assertEq(Points.get(address(this)), truePoints);
+    }
+}
