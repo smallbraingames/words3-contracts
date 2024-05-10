bd7a5fbf659d812c1adfa96ecbf02962e7843fba
smallbraingames <smallbraindev@gmail.com>
2024-05-10 12:46:59 -0700

    feat: add initial letter allocation transfer test

--- src/test/integration/Transfer.t.sol
+++ src/test/integration/Transfer.t.sol
@@ -7,6 +7,7 @@ import { Words3Test } from "../Words3Test.t.sol";
 import { Letter } from "codegen/common.sol";
 import { PlayerLetters, PriceConfigData } from "codegen/index.sol";
 import "forge-std/Test.sol";
+import { TransferLettersSystem } from "systems/TransferLettersSystem.sol";
 
 contract Transfer is Words3Test {
     function setUp() public override {
@@ -54,5 +55,98 @@ contract Transfer is Words3Test {
         assertEq(PlayerLetters.get({ player: to, letter: Letter.A }), 1);
         assertEq(PlayerLetters.get({ player: to, letter: Letter.B }), 1);
         vm.stopPrank();
+    }
+
+    function test_InitialLetterAllocationAndTransfer() public {
+        Letter[] memory initialWord = new Letter[](2);
+        initialWord[0] = Letter.H;
+        initialWord[1] = Letter.I;
+        uint32[26] memory initialLetterAllocation;
+        initialLetterAllocation[0] = 1; // 1 A
+        initialLetterAllocation[1] = 2; // 2 Bs
+        initialLetterAllocation[3] = 40; // 40 Ds
+        initialLetterAllocation[4] = 50; // 50 Es
+        initialLetterAllocation[5] = 60; // 60 Fs
+        initialLetterAllocation[25] = 2383; // 2383 Zs
+
+        address initialLettersTo = address(0x123);
+
+        world.start({
+            initialWord: initialWord,
+            initialLetterAllocation: initialLetterAllocation,
+            initialLettersTo: initialLettersTo,
+            merkleRoot: bytes32(0),
+            initialPrice: 0.001 ether,
+            priceConfig: PriceConfigData({
+                minPrice: 0.0001 ether,
+                wadFactor: 1.3e18,
+                wadDurationRoot: 2e18,
+                wadDurationScale: 3000e18,
+                wadDurationConstant: 0
+            }),
+            crossWordRewardFraction: 3,
+            bonusDistance: 3,
+            numDrawLetters: 8
+        });
+
+        // Check to see if initial letters to has the letters
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.A }), 1);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.B }), 2);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.D }), 40);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.E }), 50);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.F }), 60);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.Z }), 2383);
+
+        // Transfer some letters to a new player
+        address to = address(0x456);
+        Letter[] memory transferLetters = new Letter[](2);
+        transferLetters[0] = Letter.A;
+        transferLetters[1] = Letter.B;
+        vm.prank(initialLettersTo);
+        world.transfer({ letters: transferLetters, to: to });
+
+        // Check to see if the new player has the letters
+        assertEq(PlayerLetters.get({ player: to, letter: Letter.A }), 1);
+        assertEq(PlayerLetters.get({ player: to, letter: Letter.B }), 1);
+
+        // Check that the initial allocation player has the correct amount of letters
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.A }), 0);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.B }), 1);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.D }), 40);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.E }), 50);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.F }), 60);
+        assertEq(PlayerLetters.get({ player: initialLettersTo, letter: Letter.Z }), 2383);
+    }
+
+    function test_RevertsWhen_NoLetters() public {
+        Letter[] memory initialWord = new Letter[](2);
+        initialWord[0] = Letter.H;
+        initialWord[1] = Letter.I;
+        uint32[26] memory initialLetterAllocation;
+        world.start({
+            initialWord: initialWord,
+            initialLetterAllocation: initialLetterAllocation,
+            initialLettersTo: address(0),
+            merkleRoot: bytes32(0),
+            initialPrice: 0.001 ether,
+            priceConfig: PriceConfigData({
+                minPrice: 0.0001 ether,
+                wadFactor: 1.3e18,
+                wadDurationRoot: 2e18,
+                wadDurationScale: 3000e18,
+                wadDurationConstant: 0
+            }),
+            crossWordRewardFraction: 3,
+            bonusDistance: 3,
+            numDrawLetters: 8
+        });
+
+        address to = address(0x456);
+        Letter[] memory transferLetters = new Letter[](2);
+        transferLetters[0] = Letter.A;
+        transferLetters[1] = Letter.B;
+        vm.prank(to);
+        vm.expectRevert(TransferLettersSystem.TransferMissingLetters.selector);
+        world.transfer({ letters: transferLetters, to: to });
     }
 }
